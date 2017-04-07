3d8ea5e87f0340287a3062dc932921d8005a8133
PKirigano <pkm2009@ymail.com>
2017-04-07 14:53:39 +0300

    updated progress py

--- progress.py
+++ progress.py
@@ -1,1 +1,12 @@
 def progress():
+    my_file = open("data.txt", "r+")
+    payload = {}
+    for line in my_file:
+        task = line.split(':')
+        if task[3] == 'True\n':
+            status = task[3][:4]
+            payload[task[1]] = status
+        elif task[3] == 'False\n':
+            status = task[3][:5]
+            payload[task[1]] = status
+    return payload
